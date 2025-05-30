b057bad0cf0515b598adaafab5e0871c43225419
Kyle Mims <kylemims@gmail.com>
2025-05-30 17:44:05 -0500

    cleaning out the mess of CSS

--- src/components/forms/HoverReveal.jsx
+++ src/components/forms/HoverReveal.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./HoverReveal.css";
 
 export const HoverReveal = ({ children, revealContent, className = "" }) => {
   const [isHovering, setIsHovering] = useState(false);
@@ -15,42 +14,3 @@ export const HoverReveal = ({ children, revealContent, className = "" }) => {
     </span>
   );
 };
-
-// import { useState } from "react";
-// import "./HoverReveal.css";
-
-// export const HoverReveal = ({ children, revealContent, className = "" }) => {
-//   const [isHovering, setIsHovering] = useState(false);
-
-//   return (
-//     <span
-//       className={`hover-reveal ${className}`}
-//       style={{ position: "relative", display: "inline-block" }}
-//       onMouseEnter={() => setIsHovering(true)}
-//       onMouseLeave={() => setIsHovering(false)}>
-//       {children}
-//       {isHovering && revealContent}
-//     </span>
-//   );
-// };
-
-// import { useState } from "react";
-// import "./HoverReveal.css";
-
-// export const HoverReveal = ({ children, revealContent, className = "" }) => {
-//   const [isHovering, setIsHovering] = useState(false);
-//   const handleMouseEnter = () => setIsHovering(true);
-//   const handleMouseLeave = () => setIsHovering(false);
-
-//   return (
-//     <div className={`hover-reveal ${className}`}>
-//       <div
-//         onMouseEnter={handleMouseEnter}
-//         className="hover-reveal__content"
-//         onMouseLeave={handleMouseLeave}>
-//         {children}
-//         {isHovering && <div>{revealContent}</div>}
-//       </div>
-//     </div>
-//   );
-// };
