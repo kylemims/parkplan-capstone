27c5c2dbcd0c91d1625c35bce11edc96a51037e0
Kyle Mims <kylemims@gmail.com>
2025-06-13 13:51:59 -0500

    more work done for mobile view

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -35,11 +35,12 @@ export const Login = () => {
       </video>
       <div className="overlay">
         <form className="auth-container" onSubmit={handleLogin}>
+          <section className="welcome-section">
           <p className="login-small-title">WELCOME TO</p>
           <h1 className="login-header">Pick-A-Park</h1>
           <img src="/images/pick-logo-5.svg" alt="Pick A Park logo" className="login-logo" />
           <h2 className="subtitle">Please sign in</h2>
-
+</section>
           <fieldset>
             {/* <label htmlFor="inputEmail"> Email address </label> */}
             <input
